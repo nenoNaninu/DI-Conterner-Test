c408549e76eb1c8913ff3ebd045a2d815b4f4dde
nenoNaninu <neno@neno-garden.com>
2019-03-22 00:12:29 +0900

    check instance id

--- DiTest/Program.cs
+++ DiTest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,31 +8,39 @@ namespace DiTest
     public interface ISome1
     {
         void SaySome1();
+        Guid Id { get; set; }
+
     }
 
     public interface ISome2
     {
         void SaySome2();
+        Guid Id { get; set; }
     }
 
     public interface ISome3
     {
         void SaySome3();
+        Guid Id { get; set; }
     }
 
     public interface ISome4
     {
         void SaySome4();
+        Guid Id { get; set; }
     }
 
     public class Some1 : ISome1
     {
+        public Guid Id { get; set; }
+
         private ISome2 _some2;
         private ISome3 _some3;
         public Some1(ISome2 some2)
         {
             Console.WriteLine("construct 1");
             _some2 = some2;
+            Id = Guid.NewGuid();
         }
         
         public Some1(ISome2 some2, ISome3 some3)
@@ -39,22 +48,28 @@ namespace DiTest
             Console.WriteLine("construct 2");
             _some2 = some2;
             _some3 = some3;
+            Id = Guid.NewGuid();
+
         }
         
         public void SaySome1()
         {
             Console.WriteLine("Some1");
-            _some2.SaySome2();
+            _some2?.SaySome2();
             _some3?.SaySome3();
         }
     }
 
     public class Some2 : ISome2
     {
+        public Guid Id { get; set; }
+
         private ISome3 _some3;
         public Some2(ISome3 some3)
         {
             _some3 = some3;
+            Id = Guid.NewGuid();
+            Console.WriteLine($"Some2:{Id}");
         }
 
 
@@ -67,6 +82,15 @@ namespace DiTest
     
     public class Some3 : ISome3
     {
+        public Guid Id { get; set; }
+
+        public Some3()
+        {
+            Id = Guid.NewGuid();
+            Console.WriteLine($"Some3:{Id}");
+
+        }
+
         public void SaySome3()
         {
             Console.WriteLine("Some3");
@@ -75,6 +99,13 @@ namespace DiTest
 
     public class Some4:ISome4
     {
+        public Guid Id { get; set; }
+
+        public Some4()
+        {
+           Id = Guid.NewGuid();
+        }
+
         public void SayHai()
         {
             Console.WriteLine("Some4Hai");
@@ -90,7 +121,6 @@ namespace DiTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             var collection = new ServiceCollection();
             collection.AddTransient<ISome1, Some1>();
             collection.AddTransient<ISome2, Some2>();
@@ -100,9 +130,15 @@ namespace DiTest
             ServiceProvider provider = collection.BuildServiceProvider();
             var some1 = provider.GetService<ISome1>();
             some1.SaySome1();
-            var some4 = provider.GetService<Some4>();
-            some4.SayHai();
-
+//            var some4 = provider.GetService<Some4>();
+//            some4.SayHai();
+            
+//            Dictionary<Type, object> dictionary = new Dictionary<Type, object>();
+//            dictionary[typeof(Some1)] = some1;
+//            var hoge = some1.GetType();
+//            Some1 some1_1 = (Some1)dictionary[typeof(Some1)];
+//            some1_1.SaySome1();
+            
         }
     }
 }
